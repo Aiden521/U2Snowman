a36c99804eff6f50d31b2099b7b2555e7d7ad277
Aiden521 <36234220+Aiden521@users.noreply.github.com>
2018-03-22 15:17:05 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace U2Snowman
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            int diff = 50;
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                int Width = 100 * i + diff;
+                int Height = 50 * i + diff;
+                myStackPanel.Children.Add(drawellipse(Width, Height));
+            }
+
+            Ellipse drawellipse(double x1, double x2)
+            {
+                Ellipse myellipse = new Ellipse();
+                myellipse.Width = x1;
+                myellipse.Height = x2;
+                myellipse.Stroke = Brushes.Black;
+                return myellipse;
+
+            }
+
+        }
+    }
+}
